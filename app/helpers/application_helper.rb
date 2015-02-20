8bfcbf80c0a10acb4f910f868b887c10c9945ff2
Scott Savage <snsavage@gmail.com>
2015-02-19 21:18:00 -0500

    Cleaning up the views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+
+  # Code from https://gist.github.com/suryart/7418454
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert alert-success',
+    error: 'alert alert-error',
+    alert: 'alert alert-warning',
+    notice: 'alert alert-info'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG[flash_type.to_sym]
+  end
+
+
+
 end
