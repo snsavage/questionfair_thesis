fc57c972d6009c3e374d1b97d1d0b84f09a348fe
Scott Savage <snsavage@gmail.com>
2015-03-12 09:42:43 -0400

    Minor changes to Answers Edit View.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,6 +24,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @answers = @question.answers
     @answer = @question.answers.build
   end
 
