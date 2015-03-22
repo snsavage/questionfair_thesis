03f33acfa8ec8533ebcf3ff01f009630d3fd47bb
Scott Savage <snsavage@gmail.com>
2015-03-22 07:53:12 -0400

    Compiled Assets Change to Production Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  GA.tracker = "UA-38023509-2"
+  GA.tracker = "UA-61035764-1"
 end
