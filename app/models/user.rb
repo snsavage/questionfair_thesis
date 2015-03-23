f92eefe6cc42c1eba2428e43d84959cda4996f3e
Scott Savage <snsavage@gmail.com>
2015-03-22 20:05:08 -0400

    Fix Nickname Unique Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, source: :user
 
-  validates :nickname, uniqueness: true
+  validates :nickname, uniqueness: { case_sensitive: false }
   validates :nickname, presence: true
   validates :nickname, length: { minimum: 2 }
   validates :nickname, length: { maximum: 15 }
