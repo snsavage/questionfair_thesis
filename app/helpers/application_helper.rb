f56d81379761d18e3893959473d86570f1641cc2
Scott Savage <snsavage@gmail.com>
2015-03-26 11:44:25 -0400

    Default Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,8 +15,9 @@ module ApplicationHelper
 
   # Code from http://railscasts.com/episodes/244-gravatar
   def gravatar_url(user)
+    default_url = "#{root_url}images/badge-orange.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
 
 end
