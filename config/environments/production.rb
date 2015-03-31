7fb678abd88a933a31def77c8230de5358a911ce
Scott Savage <snsavage@gmail.com>
2015-03-31 18:49:48 -0400

    Bug Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      ENV['MANDRILL_USERNAME'],
     :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
+    :domain =>         'questionfair.com',
     :authentication => :plain
   }
 
